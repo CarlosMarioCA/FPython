a4f090b95b69c7a742ad36134fa9a82bcde4437a
CarlosMarioCA <camchavezag@unal.edu.co>
2022-03-18 16:30:54 -0500

    preFina Triki

--- Game.py
+++ Game.py
@@ -1,28 +1,51 @@
 import PySimpleGUI as sg
 
 
-button_size = (15, 10)
+button_size = (20, 10)
 PLAYER_ONE = "X"
 PLAYER_TWO = "O"
 current_player = PLAYER_ONE
+game_over = True
+sg.theme("DarkAmber")
 
-layout = [[sg.Button("", key="-1-", size=button_size), sg.Button("", key="-2-", size=button_size), sg.Button("", key="-3-", size=button_size)],
-        [sg.Button("",  key="-4-", size=button_size), sg.Button("",  key="-5-", size=button_size), sg.Button("", key="-6-", size=button_size)],
-        [sg.Button("",  key="-7-",size=button_size), sg.Button("",  key="-8-",size=button_size), sg.Button("",  key="-9-",size=button_size)],
-        [sg.Button("-GAME OVER-")]]
+deck = [0, 0, 0,
+        0, 0, 0,
+        0, 0, 0]
 
-window = sg.Window("Demo", layout, margins=(100, 100))
+win = [[0, 1, 2],[0, 3, 6],[0, 4, 8],[1, 4, 7],[2, 5, 8],[3, 4, 5],[6, 7, 8],[2, 4, 6]]
 
-while True:
+layout = [[sg.Button("", key="-0-", size=button_size), sg.Button("", key="-1-", size=button_size), sg.Button("", key="-2-", size=button_size)],
+        [sg.Button("",  key="-3-", size=button_size), sg.Button("",  key="-4-", size=button_size), sg.Button("", key="-5-", size=button_size)],
+        [sg.Button("",  key="-6-",size=button_size), sg.Button("",  key="-7-",size=button_size), sg.Button("",  key="-8-",size=button_size)],
+        [sg.Button('-GAME OVER-', 'center')]]
+
+window = sg.Window("Demo", layout, margins=(10, 10), element_justification='c')
+
+while game_over:
     event, value = window.read()
     if event == sg.WINDOW_CLOSED or event == "-GAME OVER-":
         break
 
     if window.Element(event).ButtonText == "":
+        index = int(event.replace("-", ""))
+        deck[index] = current_player
         window.Element(event).Update(text=current_player)
         if current_player == PLAYER_ONE:
             current_player = PLAYER_TWO
         else:
             current_player = PLAYER_ONE
+
+    for win_play in win:
+        if deck[win_play[0]] == deck[win_play[1]] == deck[win_play[2]] != 0:
+            print("GAME OVER.")
+            if deck[win_play[0]] == PLAYER_ONE:
+                print("Player 1 win!")
+                game_over = False
+            elif deck[win_play[0]] == PLAYER_TWO:
+                print("Player 2 win!")
+                game_over = False
+
+    if 0 not in deck:
+        print("GAME OVER.")
 
 window.Close()
---
+++ Triki.py
@@ -0,0 +1,60 @@
+import PySimpleGUI as sg
+
+
+button_size = (20, 10)
+PLAYER_ONE = "X"
+PLAYER_TWO = "O"
+current_player = PLAYER_ONE
+game_over = True
+sg.theme("DarkAmber")
+
+deck = [0, 0, 0,
+        0, 0, 0,
+        0, 0, 0]
+
+win = [[0, 1, 2],[0, 3, 6],[0, 4, 8],[1, 4, 7],[2, 5, 8],[3, 4, 5],[6, 7, 8],[2, 4, 6]]
+
+def createLayout():
+    layout = [[sg.Text("TITTLE", auto_size_text = True),],
+            [sg.Button("", key="-0-", size=button_size), sg.Button("", key="-1-", size=button_size), sg.Button("", key="-2-", size=button_size)],
+            [sg.Button("",  key="-3-", size=button_size), sg.Button("",  key="-4-", size=button_size), sg.Button("", key="-5-", size=button_size)],
+            [sg.Button("",  key="-6-",size=button_size), sg.Button("",  key="-7-",size=button_size), sg.Button("",  key="-8-",size=button_size)],
+            [sg.Button("-GAME OVER-")]]
+    return layout
+
+def createWindow():
+    layout = createLayout()
+    window = sg.Window("TRIKI 1.0", layout, margins=(10, 10), element_justification='c')
+    return window
+
+if __name__ == '__main__':
+
+    window = createWindow()
+    while game_over:
+        event, value = window.read()
+        if event == sg.WINDOW_CLOSED or event == "-GAME OVER-":
+            break
+
+        if window.Element(event).ButtonText == "":
+            index = int(event.replace("-", ""))
+            deck[index] = current_player
+            window.Element(event).Update(text=current_player)
+            if current_player == PLAYER_ONE:
+                current_player = PLAYER_TWO
+            else:
+                current_player = PLAYER_ONE
+
+        for win_play in win:
+            if deck[win_play[0]] == deck[win_play[1]] == deck[win_play[2]] != 0:
+                print("GAME OVER.")
+                if deck[win_play[0]] == PLAYER_ONE:
+                    print("Player 1 win!")
+                    game_over = False
+                elif deck[win_play[0]] == PLAYER_TWO:
+                    print("Player 2 win!")
+                    game_over = False
+
+        if 0 not in deck:
+            print("GAME OVER.")
+
+    window.Close()
